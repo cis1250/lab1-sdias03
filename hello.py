55cc3047b6c628f968231a62053adfade751b2e2
Sheldon Dias <sdias03@uoguelph.ca>
2025-09-16 11:54:25 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,4 +6,5 @@
 # 2. Add another print statement with your name.
 # 3. Add another print statement with your favorite hobby.
 
-print("Hello, world!")
+print("Sheldon Dias")
+print("My favorite hobby is playing the guitar")
